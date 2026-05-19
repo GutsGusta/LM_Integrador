59a451c5640793ade15da28928ce00743aabfe87
GutsGusta <gustavonovaes623@gmail.com>
2026-05-19 15:21:24 -0300

    Criei o Header

---
+++ partials/header.php
@@ -0,0 +1,11 @@
+    <header class="cabecalho">
+        <img src="uploads/Logo-LM.png">
+            <nav class="botoes-nav">
+                <a href="index.php">Home</a>
+                <a href="#">Serviços</a>
+                <a href="#">Profissionais</a>
+                <a href="sobre-nos.php">Sobre Nós</a>
+                <a href="#">Login</a>
+            </nav>
+            <a href="#" class="botao-orcamento">Solicitar Orçamento</a>
+    </header>
---
+++ sobre-nos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="x-icon" href="uploads/Logo-LM.png">
+    <link rel="stylesheet" href="css/sobre-nos.css">
+    <title>Sobre Nós</title>
+</head>
+<body>
+     <?php
+        require_once "partials/header.php";
+    ?> 
+
+    <main>
+
+    </main>
+</body>
+</html>
