b06f88982366e85311cbf3bb0e534c4cc9b17d40
linohash <raphalinof@gmail.com>
2026-05-22 15:06:18 -0300

    header atualizada

--- index.php
+++ index.php
@@ -14,24 +14,9 @@
 </head>
 
 <body>
-    <header>
-        <nav class="header__nav">
-            <div class="nav__left">
-                <a href="#home"><img src="uploads/Logo-LM.png" alt="LM" class="logo"></a>
-
-            </div>
-            <div class="nav__center">
-                <a class="nav__link" href="sobre-nos.php">Sobre nós</a>
-                <a class="nav__link" href="#servicos">Serviços</a>
-                <a class="nav__link" href="#equipe">Equipe</a>
-            </div>
-
-            <div class="nav__right">
-                <a href="#"><button class="icon-btn"><img src="uploads/image (1).png" width="30"
-                            height="30"></button></a>
-            </div>
-        </nav>
-    </header>
+    <?php
+        require_once "partials/header.php";
+    ?> 
 
     <section id="home" class="hero">
         <div class="texto">
@@ -187,68 +172,9 @@
         </div>
     </section>
 
-    <footer>
-        <div class="parte-cima-footer">
-            <div class="parte-cima-indv">
-                <img src="uploads/pessoas.png">
-                <div class="parte-cima-indv-txt">
-                    <p>67</p>
-                    <p>Profissionais</p>
-                </div>
-            </div>
-            <div class="parte-cima-indv">
-                <img src="uploads/casa.png">
-                <div class="parte-cima-indv-txt">
-                    <p>+500</p>
-                    <p>Serviços Prestados</p>
-                </div>
-            </div>
-            <div class="parte-cima-indv">
-                <img src="uploads/escudo.png">
-                <div class="parte-cima-indv-txt">
-                    <p>100%</p>
-                    <p>Segurança</p>
-                </div>
-            </div>
-            <div class="parte-cima-indv">
-                <img src="uploads/estrela.png">
-                <div class="parte-cima-indv-txt">
-                    <p>4,8</p>
-                    <p>Avaliação</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="parte-baixo-footer">
-            <div class="parte-baixo-indv">
-                <h3>Empresa</h3>
-                <p>Endereço:</p>
-                <p>R. Santo André, 680 - Boa Vista</p>
-                <p>(11) 4002-8922</p>
-                <p>Copyright © 2026 LM. Todos os direitos reservados.</p>
-            </div>
-
-            <div class="linha-footer"></div>
-
-            <div class="parte-baixo-indv">
-                <h3>Suporte</h3>
-                <a href="#">Ajuda</a>
-                <a href="#">Termos de Uso</a>
-                <a href="#">Privacidade</a>
-                <a href="#">Contato</a>
-            </div>
-
-            <div class="linha-footer"></div>
-
-            <div class="parte-baixo-indv">
-                <h3>Redes Sociais</h3>
-                <a href="#">Instagram</a>
-                <a href="#">TikTok</a>
-                <a href="#">Facebook</a>
-                <a href="#">Twitter</a>
-            </div>
-        </div>
-    </footer>
+   <?php
+        require_once "partials/footer.php";
+    ?> 
 
     <script>
         const faixa = document.getElementById('logos');
--- partials/header.php
+++ partials/header.php
@@ -1,11 +1,18 @@
-    <header class="cabecalho">
-        <img src="uploads/Logo-LM.png">
-            <nav class="botoes-nav">
-                <a href="index.php">Home</a>
-                <a href="servico.php">Serviços</a>
-                <a href="#">Profissionais</a>
-                <a href="sobre-nos.php">Sobre Nós</a>
-                <a href="#">Login</a>
-            </nav>
-            <a href="#" class="botao-orcamento">Solicitar Orçamento</a>
+   <header>
+        <nav class="header__nav">
+            <div class="nav__left">
+                <a href="#home"><img src="uploads/Logo-LM.png" alt="LM" class="logo"></a>
+
+            </div>
+            <div class="nav__center">
+                <a class="nav__link" href="sobre-nos.php">Sobre nós</a>
+                <a class="nav__link" href="#servicos">Serviços</a>
+                <a class="nav__link" href="#equipe">Equipe</a>
+            </div>
+
+            <div class="nav__right">
+                <a href="#"><button class="icon-btn"><img src="uploads/image (1).png" width="30"
+                            height="30"></button></a>
+            </div>
+        </nav>
     </header>
