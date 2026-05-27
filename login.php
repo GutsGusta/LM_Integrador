db96379f2e51b5415b1e638fd9c842eeba5271f5
NAYRA SANTOS AM�NCIO <25254016@educ123.sp.senai.br>
2026-05-27 16:32:49 -0300

    login feito

--- login.php
+++ login.php
@@ -4,18 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="icon" type="x-icon" href="uploads/Logo-LM.png">
     <title>Login</title>
 </head>
 <body>
 <?php
     require_once 'partials/header.php';
 ?> 
-    <form class="login">
-    <img src="./">
-    <h1>Login</h1>
-    <label>Email:<input type="email" id="email" name="email"> </label><br>
-    <label>Senha:<input type="password" id="Senha" name="Senha"></label><br>
-    <button type="submit">Entrar</button>
-</form>
+<h1 class="titulo">Bem Vindo de Volta!</h1>
+<div class="login">
+    <form class="formulario-login">
+        <img src="uploads/icone_usuario.png">       
+        <input type="email" id="email" name="email" placeholder="Email">
+        <input type="password" id="Senha" name="Senha" placeholder="Senha">
+        <button type="submit">Entrar</button>
+    </form>
+</div>
 </body>
 </html>
