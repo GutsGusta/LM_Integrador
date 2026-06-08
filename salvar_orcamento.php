7797097177ceeeb64e046fde6236b7a9ed8f7f84
danmf18 <danilo.m.filho@aluno.senai.br>
2026-06-07 22:42:59 -0300

    Solitcitação de Orcamento

---
+++ salvar_orcamento.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'crud.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $dados = [
+        'nome' => $_POST['Nome'],
+        'email' => $_POST['email'],
+        'telefone' => $_POST['Telefone'],
+        'endereco' => $_POST['Endereco'],
+        'mensagem' => $_POST['mensagem']
+    ];
+
+    create($pdo, 'orcamentos', $dados);
+
+    echo "<script>
+            alert('Orçamento enviado com sucesso!');
+            window.location.href='orcamento.php';
+          </script>";
+}
