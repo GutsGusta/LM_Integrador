1aa820279426d11b02242db7729242a458fe23ad
GutsGusta <gustavonovaes623@gmail.com>
2026-06-09 17:00:57 -0300

    Estilizei a avaliação

--- avaliar.php
+++ avaliar.php
@@ -1,5 +1,5 @@
 <?php
-require_once('data/crud.php');
+require_once 'data/crud.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -20,33 +20,43 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/avaliar.css">
+    <link rel="icon" type="x-icon" href="uploads/Logo-LM.png">
     <title>Enviar Avaliação</title>
 </head>
-<body>
-    <form action="./data/insert.php" method="POST">
-        <input type="text" name="titulo" placeholder="Digite o título" required>
-        
-        <input type="text" name="nome_servico" placeholder="Qual serviço foi realizado? (Ex: Pintura, Troca de torneira)" required>
-        
-        <input type="number" name="nota" min="1" max="5" placeholder="Nota (1 a 5)" required>
-        
-        <textarea name="texto_avaliacao" placeholder="Digite sua avaliação" rows="5" required></textarea>
-        
-        <input type="hidden" name="id_profissional" value="<?php echo ($id_profissional_avaliado); ?>">
-        <input type="hidden" name="id_cliente" value="<?php echo ($_SESSION['user_id']); ?>">
-        <input type="hidden" name="nome_profissional" value="<?php echo ($nome_profissional_avaliado); ?>">
 
-        <button type="submit">Enviar Avaliação</button>
-    </form>
+<body>
+    <?php
+        require_once 'partials/header.php';
+    ?>
+
+    <main class="pagina-principal">
+        <form action="./data/insert.php" method="POST" class="avaliacao">
+            <p>Digite o título</p>
+            <input type="text" name="titulo" required>
+            
+            <p>Qual serviço foi realizado?</p>
+            <input type="text" name="nome_servico" placeholder="(Ex: Pintura, Troca de torneira)" required>
+            
+            <p>Nota da Avaliação</p>
+            <input type="number" name="nota" min="1" max="5" placeholder="Entre 1 e 5" required>
+            
+            <p>Comentário sobre o Profissional?</p>
+            <textarea name="texto_avaliacao" rows="5" required></textarea>
+            
+            <input type="hidden" name="id_profissional" value="<?php echo ($id_profissional_avaliado); ?>">
+            <input type="hidden" name="id_cliente" value="<?php echo ($_SESSION['user_id']); ?>">
+            <input type="hidden" name="nome_profissional" value="<?php echo ($nome_profissional_avaliado); ?>">
+
+            <button type="submit">Enviar Avaliação</button>
+        </form>
+    </main>
 </body>
 </html>
